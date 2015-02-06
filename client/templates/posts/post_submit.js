92bc29e1de94eb6d5b931e6889430a523ebaa7ff
archanamutthu <archanamutthu@transnoesis.com>
2015-02-06 12:48:13 +0530

    Insertion permissions from client side alone

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -5,6 +5,7 @@ Template.postSubmit.events({
       url : $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+    //insert function will return id for the post which isto be inserted using the url is constructed
     post._id = Posts.insert(post);
     Router.go('postPage', post);
   }
