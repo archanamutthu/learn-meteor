f3f9891205e9ecc090291f3bae9a3d9b56b82a9f
archanamutthu <archanamutthu@transnoesis.com>
2015-02-06 14:33:09 +0530

    Use a method to submit

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,7 +6,19 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
     //insert function will return id for the post which isto be inserted using the url is constructed
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    //this has some drwbacks where user can insert data from console so to avoid that we are using Meteor method
+    // post._id = Posts.insert(post);
+    // Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result) {
+      if(error) {
+        return alert(error.reason);
+      }
+      Router.go('postPage', {_id: result._id});
+    })
   }
 });
+
+//Points
+// - Meteor method is server side function that is called client side
+// - Router.go is redirect the page to certain page with the id where the url can be constructed with that
+//-postInsert is called in posts collections
